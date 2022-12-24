3b7e15f406e6ec25442d2bbfd6d8e80a3abd83bc
Smolinetti <2407373@mail.ru>
2022-12-24 22:31:39 +0300

    Finished_Task_4.1

--- Task_4/index.php
+++ Task_4/index.php
@@ -19,6 +19,24 @@ $skills = [
         'percent' => 50,
     ],
 ];
+
+$experience = [
+    [
+        'works' => 'Frontend разработчик',
+        'data' => 'январь 2022 - по настоящее время',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid dolorum et eveniet fugit labore laudantium maiores minus nobis numquam obcaecati possimus, quae quaerat quibusdam recusandae, similique veniam veritatis voluptate.'
+    ],
+    [
+        'works' => 'Phyton разработчик',
+        'data' => 'январь 2021 - декабрь 2021',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid dolorum et eveniet fugit labore laudantium maiores minus nobis numquam obcaecati possimus, quae quaerat quibusdam recusandae, similique veniam veritatis voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid dolorum et eveniet fugit labore laudantium maiores minus nobis numquam obcaecati possimus, quae quaerat quibusdam recusandae, similique veniam veritatis voluptate.'
+    ],
+    [
+        'works' => 'Java разработчик',
+        'data' => 'январь 2020 - декабрь 2020',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid dolorum et eveniet fugit labore laudantium maiores minus nobis numquam obcaecati possimus, quae quaerat quibusdam recusandae, similique veniam veritatis voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid dolorum et eveniet fugit labore laudantium maiores minus nobis numquam obcaecati possimus, quae quaerat quibusdam recusandae, similique veniam veritatis voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid dolorum et eveniet fugit labore laudantium maiores minus nobis numquam obcaecati possimus, quae quaerat quibusdam recusandae, similique veniam veritatis voluptate.'
+    ],
+];
 ?>
 
 <!DOCTYPE html>
@@ -54,10 +72,10 @@ $skills = [
       <div class="w3-third">
         <div class="w3-white w3-text-grey w3-card-4">
           <div class="w3-display-container">
-            <img src="https://imgfon.ru/Images/Details_img_880px/Animals/glaza-belyy_fon-kotik-morda-polosatyy.webp"
+            <img src="https://imgfon.ru/Images/Original/Mens/vzglyad-litso-ochki-mujchina-ulyibka.jpg?img.1"
               style="width:100%" alt="Avatar">
             <div class="w3-display-bottomleft w3-container w3-text-black">
-              <h2><?=$firstname;?></h2>
+              <h2 class="w3-opacity w3-tag w3-teal w3-round"><?=$firstname;?></h2>
             </div>
           </div>
           <div class="w3-container">
@@ -102,27 +120,14 @@ $skills = [
         <div class="w3-container w3-card w3-white w3-margin-bottom">
           <h2 class="w3-text-grey w3-padding-16"><i
               class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+          <?php for($i = 0; $i < count($experience); $i++): ?>
           <div class="w3-container">
-            <h5 class="w3-opacity"><b>Front End Developer </b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span
-                class="w3-tag w3-teal w3-round">Current</span></h6>
-            <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit
-              sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+            <h5 class="w3-opacity w3-tag w3-teal w3-round"><b><?php echo $experience[$i]["works"];?> </b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $experience[$i]["data"];?>
+            <p><?php echo $experience[$i]["description"];?></p>
             <hr>
           </div>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-            <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit
-              sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-            <hr>
-          </div>
-          <div class="w3-container">
-            <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-          </div>
-        </div>
+          <?php endfor;?>
 
         <div class="w3-container w3-card w3-white">
           <h2 class="w3-text-grey w3-padding-16"><i
